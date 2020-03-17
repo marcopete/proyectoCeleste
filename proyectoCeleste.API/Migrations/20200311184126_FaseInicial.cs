9aa9590bdc77883b5b1ae45d998eb92baae840ff
unknown <marco.puga@gmail.com>
2020-03-17 17:37:30 -0300

    CreaEntidadUsuarioLogin

--- proyectoCeleste.API/Migrations/20200311184126_FaseInicial.cs
+++ proyectoCeleste.API/Migrations/20200311184126_FaseInicial.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace proyectoImagemaker.API.Migrations
+namespace proyectoCeleste.API.Migrations
 {
     public partial class FaseInicial : Migration
     {
--- proyectoCeleste.API/Migrations/20200312163253_CreaEntidadUsuario.cs
+++ proyectoCeleste.API/Migrations/20200312163253_CreaEntidadUsuario.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace proyectoImagemaker.API.Migrations
+namespace proyectoCeleste.API.Migrations
 {
     public partial class CreaEntidadUsuario : Migration
     {
